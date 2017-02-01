e567c094f3c65c58d130d0b4adbe157eb53ba146
ignitedcms <kungfuthemes@hotmail.com>
2017-02-01 21:41:30 +0000

    fixed date issue

--- application/crud_master/pmdate_edit.php
+++ application/crud_master/pmdate_edit.php
@@ -2,7 +2,7 @@
 	<label>{{val}}</label>{{required}}
 	<div class="igs-small">{{helper}}</div>
     <input class="input-sm input-s datepicker-input form-control" size="16" type="text" 
-    value="<?php echo $key->{{val}}; ?>" data-toggle="tooltip" data-placement="right" title="" data-date-format="dd-mm-yyyy" name="{{val}}" data-original-title="" readonly>
+    value="<?php echo $key->{{val}}; ?>" data-toggle="tooltip" data-placement="right" title="" data-date-format="yyyy-mm-dd" name="{{val}}" data-original-title="" readonly>
      <div class='errors'>
         <?php echo form_error('{{val}}'); ?>
     </div>

168aa8fdf01bb9b8a6589ff8e13317ecc1ce3981
ignitedcms <kungfuthemes@hotmail.com>
2017-02-24 21:10:12 +0000

    added flash data

--- application/crud_master/edit.php
+++ application/crud_master/edit.php
@@ -1,4 +1,24 @@
 <div class="pmf-container" style="margin-left:auto; margin-right:auto; margin-top:30px; min-height:800px;  ">
+    <div class="row" style="margin-left:30px; margin-right:30px;">
+        <div class="col-sm-12">
+        <?php if($this->session->flashdata('msg')) {?>
+                    
+            <?php if($this->session->flashdata('type') =='0') { ?>
+        
+            <div class="alert alert-danger">
+        
+            <?php } else {?>
+            <div class="alert alert-success">
+                <?php } ?>
+                <button type="button" class="close" data-dismiss="alert"><i class="fa fa-times"></i>
+                </button> <i class="fa fa-ban-circle"></i>
+                <?php echo $this->session->flashdata('msg');?>
+            </div>
+        <?php } ?>
+        </div>
+    </div>
+
+
     <?php $this->load->view('{{table}}/breadcrumb-edit'); ?>
     <div class="row" style="margin-left:30px; margin-right:30px;">
         <div class="col-sm-12">
